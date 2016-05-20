3da3f9339c1ca647fa4c8f91f6412e310f0a9eb8
Bryce Krah <krahbryce@yahoo.com>
2016-05-20 18:34:37 -0400

    started fetching api data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 'use strict'
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
+// downward data flow = most parent component should be the one doing data fetching
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+})
 
 const App = () => {
 
