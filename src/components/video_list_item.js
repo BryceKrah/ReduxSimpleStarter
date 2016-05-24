75569748596eb8ecfb82f5fb39c45a075ff74a1d
Bryce Krah <krahbryce@yahoo.com>
2016-05-23 22:02:16 -0400

    rendering picture and heading for videos

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+//  const video = props.video;
+// curly brackets with video is equal to if it was just props
+// like (props) and then line 4
+const imageUrl = video.snippet.thumbnails.default.url
   return (
-    <li>Video</li>
+    <li className="list-group-item">
+      <div className="video-list media">
+
+       <div className="media-left">
+        <img className="media-object" src={imageUrl}/>
+       </div>
+       <div className="media-body">
+        <div className="media-heading">
+         {video.snippet.title}
+        </div>
+       </div>
+
+      </div>
+    </li>
   )
 };
 
